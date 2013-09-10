5fbaf21397c1777d644564ccb789d674a3c24ba1
dlu <dlu@suzhoukada.com>
2013-09-10 08:59:13 +0800

    1

--- 06Gallery/gen/com/example/gallery/R.java
+++ 06Gallery/gen/com/example/gallery/R.java
@@ -21,10 +21,28 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int a1=0x7f020000;
+        public static final int a10=0x7f020001;
+        public static final int a11=0x7f020002;
+        public static final int a12=0x7f020003;
+        public static final int a13=0x7f020004;
+        public static final int a14=0x7f020005;
+        public static final int a15=0x7f020006;
+        public static final int a16=0x7f020007;
+        public static final int a2=0x7f020008;
+        public static final int a3=0x7f020009;
+        public static final int a4=0x7f02000a;
+        public static final int a5=0x7f02000b;
+        public static final int a6=0x7f02000c;
+        public static final int a7=0x7f02000d;
+        public static final int a8=0x7f02000e;
+        public static final int a9=0x7f02000f;
+        public static final int ic_launcher=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int gallery=0x7f080001;
+        public static final int switcher=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
